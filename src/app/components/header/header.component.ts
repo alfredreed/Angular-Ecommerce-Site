3f07726794be3862807c8882a1e9d83020e19fee
Alfred <reed.alfred01@gmail.com>
2025-07-09 01:56:00 -0400

    changes made to add header component

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import {Component, inject, signal} from '@angular/core';
+import {RouterLink} from '@angular/router';
+
+@Component({
+    selector: 'app-header',
+    imports: [],
+    templateUrl: 'header.component.html',
+    styleUrl: 'header.component.css'
+})
+export class HeaderComponent {
+    title = signal('Angular Ecommerce');
+}
